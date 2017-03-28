5ab8674d85e7888bb43d23931a560297c0fe8c36
StaffAuthor <StaffAuthor@outlook.com>
2017-03-28 01:23:40 -0700

    Final layoutupdated code

--- ArtCenter/App.xaml.cs
+++ ArtCenter/App.xaml.cs
@@ -28,6 +28,7 @@ namespace ArtCenter {
       this.InitializeComponent();
 			this.RequiresPointerMode = Windows.UI.Xaml.ApplicationRequiresPointerMode.WhenRequested;
 			this.Suspending += OnSuspending;
+
     }
 
     /// <summary>
