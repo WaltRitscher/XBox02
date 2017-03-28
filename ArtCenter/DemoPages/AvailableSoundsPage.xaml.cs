f41340eb5706c18a7b6d3261d3a6cb0906f6d0ba
Walt Ritscher <waltritscher@outlook.com>
2017-03-27 23:32:04 -0700

    add sound to the button click event'

--- ArtCenter/DemoPages/AvailableSoundsPage.xaml.cs
+++ ArtCenter/DemoPages/AvailableSoundsPage.xaml.cs
@@ -1,47 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace ArtCenter {
+
   /// <summary>
   /// An empty page that can be used on its own or navigated to within a Frame.
   /// </summary>
   public sealed partial class AvailableSoundsPage : Page {
-    public TabDirectionPage() {
+
+    public AvailableSoundsPage() {
       this.InitializeComponent();
     }
 
-   
-
-   
-
-    private void Button_Click(object sender, RoutedEventArgs e) {
-      var currentButton = sender as Button;
-      this.ResetButton.Focus(FocusState.Programmatic);
-      if (currentButton != null)
-      {
-        currentButton.Visibility = Visibility.Collapsed;
-      }
+    private void FocusButton_Click(object sender, RoutedEventArgs e) {
+      ElementSoundPlayer.Play(ElementSoundKind.Focus);
     }
 
-    private void ResetButton_Click(object sender, RoutedEventArgs e) {
-      foreach (Button item in MainGrid.Children.OfType<Button>())
-      {
-        item.Visibility = Visibility.Visible;
-      }
+    private void ShowButton_Click(object sender, RoutedEventArgs e) {
+      ElementSoundPlayer.Play(ElementSoundKind.Show);
+    }
+
+    private void HideButton_Click(object sender, RoutedEventArgs e) {
+      ElementSoundPlayer.Play(ElementSoundKind.Hide);
+    }
+
+    private void MoveNextButton_Click(object sender, RoutedEventArgs e) {
+      ElementSoundPlayer.Play(ElementSoundKind.MoveNext);
+    }
+
+    private void MovePrevButton_Click(object sender, RoutedEventArgs e) {
+      ElementSoundPlayer.Play(ElementSoundKind.MovePrevious);
+    }
+
+    private void GoBackButton_Click(object sender, RoutedEventArgs e) {
+      ElementSoundPlayer.Play(ElementSoundKind.GoBack);
+    }
+
+    private void InvokeButton_Click(object sender, RoutedEventArgs e) {
+      ElementSoundPlayer.Play(ElementSoundKind.Invoke);
+    }
+
+    private void ComboBox_DropCompleted(UIElement sender, DropCompletedEventArgs args) {
+      ElementSoundPlayer.Play(ElementSoundKind.Show);
+    }
+
+    private void ComboBox_DropDownOpened(object sender, object e) {
+      ElementSoundPlayer.Play(ElementSoundKind.Show);
     }
   }
 }
